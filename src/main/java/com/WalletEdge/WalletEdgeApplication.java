0d54284a0812792923087d2e12b0aa5381bea816
mohrip <m25srt@gmail.com>
2026-06-28 09:54:14 +0300

    Initial commit

---
+++ src/main/java/com/WalletEdge/WalletEdgeApplication.java
@@ -0,0 +1,13 @@
+package com.WalletEdge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletEdgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletEdgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WalletEdge/WalletEdgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WalletEdge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletEdgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
