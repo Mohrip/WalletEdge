c6b050c656fa1e9487d624c402bb38ba621d50cf
mohrip <m25srt@gmail.com>
2026-06-29 13:00:11 +0300

    a

--- src/main/java/com/WalletEdge/user/service/UserService.java
+++ src/main/java/com/WalletEdge/user/service/UserService.java
@@ -1,4 +1,45 @@
 package com.WalletEdge.user.service;
 
+import com.WalletEdge.user.dto.UserRequest;
+import com.WalletEdge.user.dto.UserResponse;
+import com.WalletEdge.user.entity.User;
+import com.WalletEdge.user.mapper.UserMapper;
+import com.WalletEdge.user.repository.UserRepository;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserResponse registerUser(UserRequest userRequest){
+        log.info("Registering user {}", userRequest.getFirstName());
+        if(userRepository.existsByEmail(userRequest.getEmail())) {
+            throw new IllegalStateException("User with email " + userRequest.getEmail() + " already exists");
+        }
+
+        User user = userMapper.toEntity(userRequest);
+        User savedUser = userRepository.save(user);
+        log.info("User saved {}", savedUser);
+        return userMapper.toResponse(savedUser);
+    }
+
+    @Transactional(readOnly = true)
+    public UserResponse getUserById(UUID id) {
+        log.info("Retrieving user by id {}", id);
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException("User with id " + id + " not found"));
+        return userMapper.toResponse(user);
+    }
 }
